f8c6640b9916833d1e70e1e7f2fbfb7f0a198ec0
vaihu8238 <vaibhavibihola@gmail.com>
2023-11-18 17:16:48 +0530

    uplode

---
+++ array in php/kry-value(array).php
@@ -0,0 +1,14 @@
+<pre>
+    <?php
+
+    
+    $cars = ["bmw"=>"x5","audi"=>"a8","ferrari"=>"spider 255","mercedeas"=>"s-class","porsche"=>"caynn","range rover"=>"evok"];
+
+    $keys = array_keys($cars);
+    $values = array_values($cars);
+    
+    print_r($keys);
+    print_r($values);
+    
+    ?>
+</pre>
---
+++ array in php/pop-push(array).php
@@ -0,0 +1,17 @@
+<pre>
+
+<?php
+
+
+$arr = ['name'=>"admin","age"=>"34","city"=>"gujarat"];
+
+print_r($arr);
+array_pop($arr);
+print_r($arr);
+
+array_push($arr,"value");
+
+print_r($arr);
+
+
+?>
